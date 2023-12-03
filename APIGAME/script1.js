10aa7e2f85777f3a33db74703253428d3610838c
musin16 <116721500+musin16@users.noreply.github.com>
2023-12-03 15:00:48 +0100

    Add files via upload

---
+++ APIGAME/script1.js
@@ -0,0 +1,67 @@
+window.onload = inicio;
+function inicio() {
+    obtenerGenero()
+    genero.addEventListener("change", function () {
+        obtenerJuego().then(obtenerImgJuegos);
+    });
+    juego.addEventListener("change", function () {
+        obtenerImgJuegos();
+    });
+}
+async function obtenerGenero() {
+    fetch("./games.json").then(respuesta => {
+        if (respuesta.ok) {
+            return respuesta.json();
+        }
+    }).then(juegos => {
+        var array = [];
+        for (let i = 0; i < juegos.length; i++) {
+            if (array.includes(juegos[i].genre.trim()))
+                continue;
+            else
+                array.push(juegos[i].genre.trim());
+        }
+        for (let i = 0; i < array.length; i++) {
+            genero.innerHTML += `<div><option value="${array[i]}">${array[i]}</option></div>`
+        }
+        obtenerJuego()
+    }).catch(error => {
+        console.log('Error:', error);
+    });
+}
+function obtenerImgJuegos() {
+    let pSelec = document.getElementById("genero").value;
+    contenido.innerHTML = "";
+    fetch("./games.json").then(respuesta => {
+        if (respuesta.ok) {
+            return respuesta.json();
+        }
+    }).then(juegos => {
+        for (let i = 0; i < juegos.length; i++) {
+            if (pSelec.includes(juegos[i].genre.trim()) && document.getElementById("juego").value.includes(juegos[i].title.trim())) {
+                contenido.innerHTML += `<img src="${juegos[i].thumbnail}"/>`;
+            }
+        }
+    }).catch(error => {
+        console.log('Error:', error);
+    });
+}
+async function obtenerJuego() {
+    let jgId = document.getElementById("juego");
+    let pSelec = document.getElementById("genero").value;
+    contenido.innerHTML = "";
+    jgId.innerHTML = "";
+    fetch("./games.json").then(respuesta => {
+        if (respuesta.ok) {
+            return respuesta.json();
+        }
+    }).then(juegos => {
+        for (let i = 0; i < juegos.length; i++) {
+            if (pSelec.includes(juegos[i].genre.trim()))
+                jgId.innerHTML += `<div><option value="${juegos[i].title.trim()}">${juegos[i].title.trim()}</option></div>`
+        }
+    }).catch(error => {
+        console.log('Error:', error);
+    }).finally(obtenerImgJuegos());
+
+}
