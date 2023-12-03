469eec69816e3f4f787727c7915dd52061686f3c
musin16 <116721500+musin16@users.noreply.github.com>
2023-12-03 15:28:53 +0100

    Add files via upload

---
+++ APIGAME1SELECT/script1.js
@@ -0,0 +1,44 @@
+window.onload = inicio;
+function inicio() {
+    obtenerGenero()
+    genero.addEventListener("change", function () {
+       obtenerImgJuegos();
+    });
+}
+async function obtenerGenero() {
+    fetch("./games.json").then(respuesta => {
+        if (respuesta.ok) {
+            return respuesta.json();
+        }
+    }).then(juegos => {
+        var array = [];
+        for (let i = 0; i < juegos.length; i++) {
+            if (array.includes(juegos[i].genre.trim()))
+                continue;
+            else
+                array.push(juegos[i].genre.trim());
+        }
+        for (let i = 0; i < array.length; i++) {
+            genero.innerHTML += `<div><option value="${array[i]}">${array[i]}</option></div>`
+        }
+    }).catch(error => {
+        console.log('Error:', error);
+    });
+}
+function obtenerImgJuegos() {
+    let pSelec = document.getElementById("genero").value;
+    contenido.innerHTML = "";
+    fetch("./games.json").then(respuesta => {
+        if (respuesta.ok) {
+            return respuesta.json();
+        }
+    }).then(juegos => {
+        for (let i = 0; i < juegos.length; i++) {
+            if (pSelec.includes(juegos[i].genre.trim())) {
+                contenido.innerHTML+= `<img src="${juegos[i].thumbnail}"/>`;
+            }
+        }
+    }).catch(error => {
+        console.log('Error:', error);
+    });
+}
